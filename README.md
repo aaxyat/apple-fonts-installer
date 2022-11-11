d774f443f444f67ef909990d3e3a87d73f2f5a8b
Ayush Bhattarai <mail@ayushb.com>
2022-11-11 10:49:39 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Apple Developer Fonts Installer
+
+This scripts installs Apple's San Fransisco and New York Fonts on Linux.
+
+
+
+## Requirements
+To run this script, you must have 7zip and curl installed on your system.
+## Installation
+
+To run the script, 
+
+```bash
+  git clone
+  cd
+  ./install.sh
+```
+Or, just paste this command on your terminal
+
+```bash
+ 
+```
+
+
+
+## License
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://www.gnu.org/licenses/gpl-3.0.txt)
+
+## Acknowledgements
+ - [Apple Developer Fonts](https://developer.apple.com/fonts/)
+ - [apple-fonts AUR](https://aur.archlinux.org/packages/apple-fonts)
+ 
---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+pkgdir=/tmp/fontinstaller
+pkgs='p7zip-full curl'
+install=false
+for pkg in $pkgs; do
+  status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
+  if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
+    install=true
+    break
+  fi
+done
+if "$install"; then
+ echo 'Please Install' "$pkg" 'for your distro'
+exit 1
+
+ 
+fi
+mkdir -p $pkgdir
+mkdir -p $pkgdir/fontfiles/
+cd $pkgdir || exit
+
+curl -O https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg
+7z x SF-Pro.dmg
+cd SFProFonts || exit
+7z x 'SF Pro Fonts.pkg'
+7z x 'Payload~'
+mv Library/Fonts/* $pkgdir/fontfiles
+cd ..
+
+curl -O https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg
+7z x SF-Compact.dmg
+cd SFCompactFonts || exit
+7z x 'SF Compact Fonts.pkg'
+7z x 'Payload~'
+mv Library/Fonts/* $pkgdir/fontfiles
+cd ..
+
+curl -O https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg
+7z x SF-Mono.dmg
+cd SFMonoFonts || exit
+7z x 'SF Mono Fonts.pkg'
+7z x 'Payload~'
+mv Library/Fonts/* $pkgdir/fontfiles
+cd ..
+
+curl -O https://devimages-cdn.apple.com/design/resources/download/SF-Arabic.dmg
+7z x SF-Arabic.dmg
+cd SFArabicFonts || exit
+7z x 'SF Arabic Fonts.pkg'
+7z x 'Payload~'
+mv Library/Fonts/* $pkgdir/fontfiles
+cd ..
+
+curl -O https://devimages-cdn.apple.com/design/resources/download/NY.dmg
+7z x NY.dmg
+cd NYFonts || exit
+7z x 'NY Fonts.pkg'
+7z x 'Payload~'
+mv Library/Fonts/* $pkgdir/fontfiles
+cd ..
+
+sudo mkdir -p /usr/share/fonts/applefonts/
+sudo mv $pkgdir/fontfiles/* /usr/share/fonts/applefonts/
+sudo rm -rf /tmp/fontinstaller/
+fc-cache -f -v
